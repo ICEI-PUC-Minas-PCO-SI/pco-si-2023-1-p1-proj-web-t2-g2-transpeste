9dc675d7807e9735d7fcb60731cbd06558e06aa1
Ana Clara Marçal <limaclaraana15@gmail.com>
2023-06-26 15:24:52 -0300

    alterações para editar os dados de cadastro

--- src/loginusuario/editar.js
+++ src/loginusuario/editar.js
@@ -4,58 +4,51 @@ const inputnome = document.querySelector('input#nome')
 const inputtelefone = document.querySelector('input#telefone')
 const inputemail = document.querySelector('input#email')
 
-const urlresponsavel= "https://crud-server-json-trans-peste.vercel.app/responsaveis"
-const urlmotoristas= "https://crud-server-json-trans-peste.vercel.app/motoristas"
+const urlresponsavel = "https://crud-server-json-trans-peste.vercel.app/responsaveis"
+
 const urlinteira = new URLSearchParams(window.location.search)
 const idurl = urlinteira.get('id')
 
-async function DadosResponsavel(){
-    console.log(urlresponsavel+'/'+idurl)
-    const dadosnaURL = await fetch(urlresponsavel+'/'+idurl)
-    console.log (dadosnaURL)
-    const dadosquepegueijason = await dadosnaURL.json()
+async function DadosResponsavel() {
+  console.log(urlresponsavel + '/' + idurl)
+  const dadosnaURL = await fetch(urlresponsavel + '/' + idurl)
+  console.log(dadosnaURL)
+  const dadosquepegueijason = await dadosnaURL.json()
 
-    inputnome.value = dadosquepegueijason.nome +' '+ dadosquepegueijason.sobrenome
-    inputtelefone.value = dadosquepegueijason.telefone 
-    inputemail.value = dadosquepegueijason.email
+  inputnome.value = dadosquepegueijason.nome + ' ' + dadosquepegueijason.sobrenome
+  inputtelefone.value = dadosquepegueijason.telefone
+  inputemail.value = dadosquepegueijason.email
 }
 DadosResponsavel()
 
-function EditarResponsavel(){
-    const nascimento = document.getElementById('nascimento').value;
-    const cpf = document.getElementById('cpf').value;
-    const endereco = document.getElementById('endereço').value;
+function salvar() {
+  const nascimento = document.getElementById('nascimento').value;
+  const cpf = document.getElementById('cpf').value;
+  const endereco = document.getElementById('endereço').value;
 
-    axios
-    .get('https://crud-server-json-trans-peste.vercel.app/responsaveis?nascimento=' + nascimento)
-    .get('https://crud-server-json-trans-peste.vercel.app/responsaveis?cpf=' + cpf)
-    .get('https://crud-server-json-trans-peste.vercel.app/responsaveis?endereco=' + endereco)
+  const responsavel = {
+    "id": idurl,
+    "nome": inputnome.value,
+    "telefone": inputtelefone.value,
+    "email": inputemail.value,
+    "nascimento": nascimento,
+    "cpf": cpf,
+    "endereco": endereco
+  };
 
-    const responsavel = {
-        nascimento:nascimento,
-        cpf: cpf,
-        endereco: endereco
-      };
+  var url = `https://crud-server-json-trans-peste.vercel.app/responsaveis`;
 
-      axios.put('https://crud-server-json-trans-peste.vercel.app/responsaveis', responsavel)
-      .then(function(responseCadastro) {
-        alert('Informações atualizadas com sucesso!');
-        // Redireciona para a página do responsável
-        window.location.href = '../loginusuario/perfilusuario.html';
-      })
-      .catch(function(error) {
-        alert('Informações atualizadas com sucesso!');
-        console.error('Erro ao salvar informações:', error);
-        // Salva as informações mesmo em caso de erro
-        window.location.href = '../loginusuario/perfilusuario.html';
-      });
-
-}
-
-function salvar(){
-    const nome = inputnome.value 
-    const telefone = inputtelefone.value
-    const email = inputemail.value
+  fetch(url, {
+    method: "PUT",
+    body: JSON.stringify(responsavel),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(response => {
+    location.reload();
+  }).catch(error => {
+    console.log(error);
+  });
 }
 
 
@@ -67,21 +60,20 @@ function salvar(){
 
 
 
+// async function DadosMotoristas(){
+//     console.log(urlmotoristas+'/'+idurl)
+//     const dadosnaURL = await fetch(urlmotoristas+'/'+idurl)
+//     console.log (dadosnaURL)
+//     const dadosquepegueijason = await dadosnaURL.json()
 
-async function DadosMotoristas(){
-    console.log(urlmotoristas+'/'+idurl)
-    const dadosnaURL = await fetch(urlmotoristas+'/'+idurl)
-    console.log (dadosnaURL)
-    const dadosquepegueijason = await dadosnaURL.json()
+//     inputnome.value = dadosquepegueijason.nome +' '+ dadosquepegueijason.sobrenome
+//     inputtelefone.value = dadosquepegueijason.telefone
+//     inputemail.value = dadosquepegueijason.email
+// }
+// DadosMotoristas()
 
-    inputnome.value = dadosquepegueijason.nome +' '+ dadosquepegueijason.sobrenome
-    inputtelefone.value = dadosquepegueijason.telefone 
-    inputemail.value = dadosquepegueijason.email
-}
-DadosMotoristas()
-
-function salvar(){
-    const nome = inputnome.value 
-    const telefone = inputtelefone.value
-    const email = inputemail.value
-}
+// function salvar(){
+//     const nome = inputnome.value
+//     const telefone = inputtelefone.value
+//     const email = inputemail.value
+// }
