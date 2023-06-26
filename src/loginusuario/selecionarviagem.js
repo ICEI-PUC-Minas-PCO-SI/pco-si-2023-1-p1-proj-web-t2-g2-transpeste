1422b1206eb23e8729b83a8cd410eb21ce8cd2da
Otavio0899 <otavioscosta99@gmail.com>
2023-06-25 22:34:01 -0300

    Telas de solicitação de viagens do usuário

---
+++ src/loginusuario/selecionarviagem.js
@@ -0,0 +1,29 @@
+const url = "https://crud-server-json-trans-peste.vercel.app/solicitacao";
+const caixa_sugestoes = document.getElementById("caixa_sugestoes");
+const dadosurl = new URLSearchParams(window.location.search);
+const urlID = dadosurl.get("id");
+
+async function preencher(){
+    const preencherDados = await fetch(url + "/" + urlID);
+    const preencherDadosJson = await preencherDados.json();   
+    console.log(urlID);
+
+        const h2 = document.createElement("h2");
+        h2.innerHTML = preencherDadosJson.solicitante;
+        const p = document.createElement("p");
+        p.innerHTML = "<strong>Rota: </strong>" + preencherDadosJson.escola + "<br>" + "<strong>Partida: </strong>" + preencherDadosJson.partida;
+        const aceitar = document.createElement("button1");
+        aceitar.setAttribute("id","button1") 
+        aceitar.innerHTML = "Aceitar";
+        const recusar = document.createElement("button2");
+        recusar.setAttribute("id","button2");
+        recusar.innerHTML = "Recusar";
+        const div = document.createElement("div");
+        div.appendChild(h2);
+        div.appendChild(p);
+        div.appendChild(aceitar);
+        div.appendChild(recusar);
+        caixa_sugestoes.appendChild(div);
+        
+}
+preencher()
